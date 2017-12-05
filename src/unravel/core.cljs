c100cefa24ec88e2271a808b392f755b06f69850
Paulus Esterhazy <pesterhazy@gmail.com>
2017-12-05 12:44:06 +0100

    Refactor arg handling

--- src/unravel/core.cljs
+++ src/unravel/core.cljs
@@ -26,21 +26,52 @@
                               :flags (spec/& (spec/cat :_ #{"--flag"} :path string?) (spec/conformer #(:path %)))))
    :host (spec/? string?) :port (spec/and string? #(re-matches #"\d+" %))))
 
+(defn parse-args [args]
+  (loop [m {}
+         [arg nxt :as args] args]
+    (cond
+      (nil? arg)
+      m
+
+      (= "--version" arg)
+      (recur (assoc m :version? true) (rest args))
+
+      (= "--debug" arg)
+      (recur (assoc m :debug? true) (rest args))
+
+      (#{"--classpath" "-c"} arg)
+      (do
+        (assert (some? nxt) "Needs parameter")
+        (recur (update m :cp (fn [elements] (conj (or elements []) nxt))) (rest (rest args))))
+
+      (#{"--blob"} arg)
+      (do
+        (assert (some? nxt) "Needs parameter")
+        (recur (update m :blobs (fn [elements] (conj (or elements []) nxt))) (rest (rest args))))
+
+      (#{"--flag"} arg)
+      (do
+        (assert (some? nxt) "Needs parameter")
+        (recur (update m :flags (fn [elements] (conj (or elements #{}) nxt))) (rest (rest args))))
+
+      (.startsWith arg "-")
+      (throw (js/Error. (str "Unknown argument: " arg)))
+
+      :else
+      (recur (update m :positional (fn [xs] (conj (or xs []) arg)))
+             (rest args)))))
+
 (def help-text
   "Syntax: unravel [--debug] [-c|--classpath <paths>] [--blob blob1 [--blob blob2 ...]] [--flag flag1 [--flag --flag2 ...]] [<host>] <port>\n        unravel --version")
 
 (defn -main [& more]
   (init)
-  (let [{:keys [options host port]}
-        (into {} (doto (spec/conform ::cmdline-args more)
-                   (some-> #{::spec/invalid}
-                           (when (fail help-text)))))
-        {:keys [cp version debug blobs flags]}
-        (transduce (map (fn [[tag v]] {tag [v]})) (partial merge-with into) {} options)]
-    (when version (print-version!))
-    (when debug (reset! ul/debug? true))
+  (let [{:keys [version? debug? positional] :as args} (parse-args more)
+        [host port] positional]
+    (when version? (print-version!))
+    (when debug? (reset! ul/debug? true))
+    (when-not (= 2 (count positional))
+      (throw (js/Errorr. "You need to pass exactly 2 arguments")))
     (uo/start (or host "localhost")
               port
-              {:cp (into [] (comp (mapcat #(str/split % (re-pattern (.-delimiter un/path)))) (distinct)) cp)
-               :flags (into #{} (comp (mapcat #(re-seq #"(?:([^\s,/]+)/)?([^\s,]+)" %)) (map (fn [[_ ns name]] (keyword ns name)))) flags)
-               :blobs blobs})))
+              args)))
